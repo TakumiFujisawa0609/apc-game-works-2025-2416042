5eb1dd23ffb9cba1d1f0f99ef7d5a80da67562bc
do-mo <2416042@s.asojuku.ac.jp>
2025-11-18 14:50:13 +0900

    no message

--- Src/Scene/GameScene.cpp
+++ Src/Scene/GameScene.cpp
@@ -274,11 +274,11 @@ void GameScene::Update(void)
 	
 
 		// --- キーボード操作（既存） ---
-		if (inputManager_.IsTrgDown(KEY_INPUT_W))
+		if (inputManager_.IsTrgDown(KEY_INPUT_A))
 		{
 			selectedChoice_ = (selectedChoice_ == 0) ? (int)questions_[questionIndex_].choices.size() - 1 : selectedChoice_ - 1;
 		}
-		else if (inputManager_.IsTrgDown(KEY_INPUT_S))
+		else if (inputManager_.IsTrgDown(KEY_INPUT_D))
 		{
 			selectedChoice_ = (selectedChoice_ == (int)questions_[questionIndex_].choices.size() - 1) ? 0 : selectedChoice_ + 1;
 		}
